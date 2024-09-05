e6cfb93e7efa0f1f8dedd14d7c95bdc804eec909
Alexandro Tapia <zpaceway@gmail.com>
2024-09-05 05:21:25 -0500

    add cache

--- nodes/views.py
+++ nodes/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 import requests
 import json
 import os
@@ -15,6 +16,7 @@ import shlex
 import shutil
 from core.utils import auto_retry
 import mimetypes
+from cachetools import TTLCache
 
 
 def download_file_chunk(chunk_name, chunk_object, temp_dir_path):
@@ -61,6 +63,69 @@ browser_mime_types = [
     "application/xml",
     "application/xhtml+xml",
 ]
+
+MAX_CACHE_SIZE = 5 * 1024 * 1024 * 1024
+cache = TTLCache(maxsize=MAX_CACHE_SIZE, ttl=300)
+
+
+def get_file_data_from_node_id(node_id: str):
+    if node_id in cache:
+        return cache[node_id]
+
+    node = Node.objects.get(id=node_id)
+    content = json.loads(node.data)
+    chunk_objects = content["chunks"]
+    compressed = content["compressed"]
+
+    temp_dir_path = os.path.join(settings.BASE_DIR, "tmp", uuid.uuid4().__str__())
+    os.makedirs(temp_dir_path, exist_ok=True)
+
+    master_chunk_name: str = None
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers=8) as executor:
+        futures = []
+        for chunk_object in chunk_objects:
+            chunk_name = chunk_object["name"]
+            if not compressed or "001" == chunk_name[-3::]:
+                master_chunk_name = chunk_name
+
+            futures.append(
+                executor.submit(
+                    download_file_chunk, chunk_name, chunk_object, temp_dir_path
+                )
+            )
+
+        for future in futures:
+            future.result()
+
+    if compressed:
+        unzip_result_dir_path = os.path.join(temp_dir_path, "result")
+        master_zip_path = shlex.quote(os.path.join(temp_dir_path, master_chunk_name))
+
+        os.makedirs(unzip_result_dir_path, exist_ok=True)
+
+        unzip_command = f"7z x {master_zip_path} -o{unzip_result_dir_path}"
+        os.system(unzip_command)
+
+        result_file_name = os.listdir(unzip_result_dir_path)[0]
+        result_file_path = os.path.join(unzip_result_dir_path, result_file_name)
+
+    else:
+        result_file_path = os.path.join(temp_dir_path, os.listdir(temp_dir_path)[0])
+
+    mime_type, _ = mimetypes.guess_type(result_file_path) or "application/octet-stream"
+    data = None
+
+    with open(result_file_path, "rb") as f:
+        data = f.read()
+
+    shutil.rmtree(temp_dir_path)
+
+    result = [data, mime_type, node.name]
+
+    cache[node_id] = result
+
+    return result
 
 
 class NodesView(views.APIView):
@@ -118,59 +183,17 @@ class NodesDownloadView(views.APIView):
     serializer_class = None
 
     def get(self, _, node_id: str):
-        node = Node.objects.get(id=node_id)
-        content = json.loads(node.data)
-        chunk_objects = content["chunks"]
-        compressed = content["compressed"]
+        result = get_file_data_from_node_id(node_id)
 
-        temp_dir_path = os.path.join(settings.BASE_DIR, "tmp", uuid.uuid4().__str__())
-        os.makedirs(temp_dir_path, exist_ok=True)
+        data, mime_type, filename = result
 
-        master_chunk_name: str = None
-
-        with concurrent.futures.ProcessPoolExecutor(max_workers=8) as executor:
-            futures = []
-            for chunk_object in chunk_objects:
-                chunk_name = chunk_object["name"]
-                if not compressed or "001" == chunk_name[-3::]:
-                    master_chunk_name = chunk_name
-
-                futures.append(
-                    executor.submit(
-                        download_file_chunk, chunk_name, chunk_object, temp_dir_path
-                    )
-                )
-
-            for future in futures:
-                future.result()
-
-        if compressed:
-            unzip_result_dir_path = os.path.join(temp_dir_path, "result")
-            master_zip_path = shlex.quote(
-                os.path.join(temp_dir_path, master_chunk_name)
-            )
-
-            os.makedirs(unzip_result_dir_path, exist_ok=True)
-
-            unzip_command = f"7z x {master_zip_path} -o{unzip_result_dir_path}"
-            os.system(unzip_command)
-
-            result_file_name = os.listdir(unzip_result_dir_path)[0]
-            result_file_path = os.path.join(unzip_result_dir_path, result_file_name)
-
-        else:
-            result_file_path = os.path.join(temp_dir_path, os.listdir(temp_dir_path)[0])
-
-        mime_type, _ = (
-            mimetypes.guess_type(result_file_path) or "application/octet-stream"
+        response = HttpResponse(
+            data,
         )
-        response = FileResponse(
-            open(result_file_path, "rb"),
-            as_attachment=mime_type not in browser_mime_types,
-            filename=node.name,
+
+        response["Content-Disposition"] = (
+            f'{"inline" if mime_type in browser_mime_types else "attachment"}; filename="{filename}"'
         )
         response["Content-Type"] = mime_type
-
-        shutil.rmtree(temp_dir_path)
 
         return response
