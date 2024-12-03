77f37176bf7d78b068e1d9234e6dea2b1435bced
Alexandro Tapia <zpaceway@gmail.com>
2024-12-03 13:39:31 -0500

    debug true by default

--- fileship/settings.py
+++ fileship/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "false") == "true"
+DEBUG = os.getenv("DEBUG", "true") == "true"
 
 ALLOWED_HOSTS = [
     "localhost",
