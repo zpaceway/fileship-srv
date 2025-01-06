853f434faa9e9eea76f6d93d163adaa54201c589
Alexandro Tapia <zpaceway@gmail.com>
2025-01-06 03:51:35 -0500

    update

--- fileship/urls.py
+++ fileship/urls.py
@@ -1,24 +1,12 @@
-from django.conf import settings
 from django.urls import path, include
-from django.conf.urls.static import static
 from core.views import OTPRequestView, OTPValidateView, UserView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 
-urlpatterns = (
-    [
-        path("api/users/", UserView.as_view()),
-        path("api/users/otp/request/", OTPRequestView.as_view()),
-        path("api/users/otp/validate/", OTPValidateView.as_view()),
-        path("api/users/token/refresh/", TokenRefreshView.as_view()),
-        path("api/buckets/", include("buckets.urls")),
-    ]
-    + static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT,
-    )
-    + static(
-        settings.STATIC_URL,
-        document_root=settings.STATIC_ROOT,
-    )
-)
+urlpatterns = [
+    path("srv/api/users/", UserView.as_view()),
+    path("srv/api/users/otp/request/", OTPRequestView.as_view()),
+    path("srv/api/users/otp/validate/", OTPValidateView.as_view()),
+    path("srv/api/users/token/refresh/", TokenRefreshView.as_view()),
+    path("srv/api/buckets/", include("buckets.urls")),
+]
--- fileship/wsgi.py
+++ fileship/wsgi.py
@@ -10,7 +10,12 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
+from whitenoise import WhiteNoise  # type: ignore
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fileship.settings")
 
-application = get_wsgi_application()
+wsgi_app = get_wsgi_application()
+application = WhiteNoise(wsgi_app, root="/app/static", prefix=settings.STATIC_URL)
+application.add_files("/app/media", prefix=settings.MEDIA_URL)
